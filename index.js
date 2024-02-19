9c79c3a92b3838a3c18aedbdd62574d3805acdb3
Spencer <spinsab@gmail.com>
2024-02-19 15:26:08 -0500

    arithmetic in JS lab

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
+{
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+function multiplyNumbers() {
+    return multiply;
+
+
+
+}
+}
+
+{
+const random = Math.floor(Math.random() * 10) + 1;
+
+function randomInteger() {
+    return random;
+}
+}
+
+{
+const num3 = 12;
+const num4 = 3;
+const mod = num3 / num4;
+
+function divideNumbers() {
+    return mod;
+}
+}
+
+{
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
+
+function maxNumber () {
+    return max;
+}
+}
